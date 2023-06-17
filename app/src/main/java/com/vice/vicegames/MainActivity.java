54b8eeab2db0e41a3756eea4a3f6b4c60d5049b9
Dmitry Korolev <k-d-a-200@mail.ru>
2023-06-17 22:37:36 +0300

    OkHttp

--- app/src/main/java/com/vice/vicegames/MainActivity.java
+++ app/src/main/java/com/vice/vicegames/MainActivity.java
@@ -1,11 +1,20 @@
 package com.vice.vicegames;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +23,27 @@ public class MainActivity extends AppCompatActivity {
     Button buttonRegister;
 
     private void showSignInWindow(){
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://vicegames.ru/integration/userinfo?token=1&email=1&device_name=1";
 
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String responseBody = response.body().string();
+                    if(responseBody.length() == 0){
+                        responseBody.getBytes();
+                    }
+                }
+            }
+        });
     }
 
     private void showRegisterWindow(){
