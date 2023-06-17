9e82d3556e5787abec78020f6c1a2ed94d72e340
Dmitry Korolev <k-d-a-200@mail.ru>
2023-06-17 19:14:19 +0300

    Add Vice design

--- app/src/main/java/com/vice/vicegames/MainActivity.java
+++ app/src/main/java/com/vice/vicegames/MainActivity.java
@@ -3,12 +3,46 @@ package com.vice.vicegames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout activityMainLayout;
+    Button buttonSignIn;
+    Button buttonRegister;
+
+    private void showSignInWindow(){
+
+    }
+
+    private void showRegisterWindow(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityMainLayout = findViewById(R.id.activityMainLayout);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignInWindow();
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterWindow();
+            }
+        });
+
     }
 }
